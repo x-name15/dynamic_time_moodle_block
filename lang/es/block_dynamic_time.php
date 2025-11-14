157c3bade9e7c25865c9a7c91fa6e7e870217e3b
Felix Manrique | Flez | Mr Jacket <69986110+x-name15@users.noreply.github.com>
2025-11-14 10:11:57 +0100

    feat(lang): add missing capability string for myaddinstance

--- lang/es/block_dynamic_time.php
+++ lang/es/block_dynamic_time.php
@@ -5,8 +5,8 @@
  * @copyright  2025
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 $string['pluginname'] = 'Bloque de Hora Dinámica';
 $string['labeltime'] = 'La hora actual es:';
 $string['loadingtime'] = 'Cargando hora...';
 $string['dynamic_time:addinstance'] = 'Agregar un nuevo bloque de hora dinámica';
+$string['dynamic_time:myaddinstance'] = 'Agregar un nuevo bloque de hora dinámica al panel de Moodle';
